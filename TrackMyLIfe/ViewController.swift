63ba7e271448b3b6bdcea58fbd9a717739bdf095
Ai-Lyn Tang <ai-lyntang@192-168-1-3.tpgi.com.au>
2017-09-30 12:24:19 +1000

    Initial project setup

---
+++ TrackMyLIfe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrackMyLIfe
+//
+//  Created by Ai-Lyn Tang on 30/9/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
